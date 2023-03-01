26b6fa6a75d2e4b79ff28cd4c8a75402501bc7d5
Dewey Watkins <deweywatkins77@gmail.com>
2023-03-01 18:22:56 -0500

    search button saves city if not already existing to local cache, page also displays saved citie

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-// const url = "https://api.openweathermap.org/data/2.5/forecast?appid=c4bc6124ae143bb4ad40a5546c016dd6&units=imperial&q=woodstock,GA,1&cnt=20"
-
 // current weather = https://api.openweathermap.org/data/2.5/weather?appid=c4bc6124ae143bb4ad40a5546c016dd6&units=imperial&q=Atlanta,Ga,1
 // forecast = https://api.openweathermap.org/data/2.5/forecast?appid=c4bc6124ae143bb4ad40a5546c016dd6&units=imperial&q=woodstock,GA,1&cnt=20
 // fetch(url)
@@ -10,3 +8,36 @@
 //     .then (function (data){
 //         console.log(data)
 //     })
+
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?appid=c4bc6124ae143bb4ad40a5546c016dd6"
+const currentURL = "https://api.openweathermap.org/data/2.5/weather?appid=c4bc6124ae143bb4ad40a5546c016dd6"
+const submitEl = document.querySelector('#searchBtn')
+const cityListEl = document.querySelector('.cityList')
+var savedCity = JSON.parse(localStorage.getItem("cityCache"))
+
+//set savedCity if it doesn't exist
+if (!savedCity){savedCity=[]}
+
+//event listner for search button
+submitEl.addEventListener("click", function(event){
+    event.preventDefault()
+    var searchValue = document.querySelector('#searchValue').value
+    //only add city if it doesn't exist
+    if (!savedCity.includes(searchValue)){
+        savedCity.unshift(searchValue)
+    }
+    localStorage.setItem("cityCache", JSON.stringify(savedCity))
+    populateCities()
+})
+
+// function for populating saved cities
+function populateCities(){
+    cityListEl.replaceChildren()
+    savedCity.forEach((element)=>{
+        var cityListItem = document.createElement("p")
+        cityListItem.textContent = element
+        cityListEl.appendChild(cityListItem)
+    })
+}
+
+populateCities()
