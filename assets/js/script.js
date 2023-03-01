45515e3a170d46f8ddba0c375acaf3439e35f5c1
Dewey Watkins <deweywatkins77@gmail.com>
2023-03-01 17:03:55 -0500

    html framework laid out, started javascript

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// const url = "https://api.openweathermap.org/data/2.5/forecast?appid=c4bc6124ae143bb4ad40a5546c016dd6&units=imperial&q=woodstock,GA,1&cnt=20"
+
+// current weather = https://api.openweathermap.org/data/2.5/weather?appid=c4bc6124ae143bb4ad40a5546c016dd6&units=imperial&q=Atlanta,Ga,1
+// forecast = https://api.openweathermap.org/data/2.5/forecast?appid=c4bc6124ae143bb4ad40a5546c016dd6&units=imperial&q=woodstock,GA,1&cnt=20
+// fetch(url)
+//     .then(function (response) {
+//         console.log(response)
+//         return response.json()
+//     })
+//     .then (function (data){
+//         console.log(data)
+//     })
