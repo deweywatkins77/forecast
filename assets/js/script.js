b63611adce79b17c9a3909cc904ae7362decad89
Dewey Watkins <deweywatkins77@gmail.com>
2023-03-02 08:42:46 -0500

    fixed submit event listner, now using nested objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 // current weather = https://api.openweathermap.org/data/2.5/weather?appid=c4bc6124ae143bb4ad40a5546c016dd6&units=imperial&q=Atlanta,Ga,1
 // forecast = https://api.openweathermap.org/data/2.5/forecast?appid=c4bc6124ae143bb4ad40a5546c016dd6&units=imperial&q=woodstock,GA,1&cnt=20
-// fetch(url)
+// 
+//geo =http://api.openweathermap.org/geo/1.0/direct?q=canton,ga,1&appid=c4bc6124ae143bb4ad40a5546c016dd6
+//fetch(url)
 //     .then(function (response) {
 //         console.log(response)
 //         return response.json()
@@ -11,33 +13,41 @@
 
 const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?appid=c4bc6124ae143bb4ad40a5546c016dd6"
 const currentURL = "https://api.openweathermap.org/data/2.5/weather?appid=c4bc6124ae143bb4ad40a5546c016dd6"
+const geoURL = "http://api.openweathermap.org/geo/1.0/direct?appid=c4bc6124ae143bb4ad40a5546c016dd6"
 const submitEl = document.querySelector('#searchBtn')
 const cityListEl = document.querySelector('.cityList')
-var savedCity = JSON.parse(localStorage.getItem("cityCache"))
+var savedCities = JSON.parse(localStorage.getItem("cityCache"))
 
 //set savedCity if it doesn't exist
-if (!savedCity){savedCity=[]}
+if (!savedCities){savedCities={}}
 
 //event listner for search button
 submitEl.addEventListener("click", function(event){
     event.preventDefault()
-    var searchValue = document.querySelector('#searchValue').value
+    var Name = document.querySelector('#searchValue').value
+    var locName={}
+    locName.City = document.querySelector('#searchValue').value.split(',')[0].trim()
+    locName.State = document.querySelector('#searchValue').value.split(',')[1].trim()
     //only add city if it doesn't exist
-    if (!savedCity.includes(searchValue)){
-        savedCity.unshift(searchValue)
-    }
-    localStorage.setItem("cityCache", JSON.stringify(savedCity))
+    if (!savedCities.Name){savedCities[Name] = locName}
+    localStorage.setItem("cityCache", JSON.stringify(savedCities))
     populateCities()
 })
 
 // function for populating saved cities
 function populateCities(){
     cityListEl.replaceChildren()
-    savedCity.forEach((element)=>{
+    Object.keys(savedCities).forEach((element)=>{
+        console.log(savedCities[element])
         var cityListItem = document.createElement("p")
-        cityListItem.textContent = element
+        cityListItem.textContent = savedCities[element].City + ", " + savedCities[element].State
         cityListEl.appendChild(cityListItem)
     })
+}
+
+//function for getting lat and long
+function getCoord(city, state){
+
 }
 
 populateCities()
