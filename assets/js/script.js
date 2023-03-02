a1edf7ccaba057c60039b8201df456a9d3d42274
Dewey Watkins <deweywatkins77@gmail.com>
2023-03-02 09:56:02 -0500

    fetching coordinates for city state is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,23 +22,30 @@ var savedCities = JSON.parse(localStorage.getItem("cityCache"))
 if (!savedCities){savedCities={}}
 
 //event listner for search button
-submitEl.addEventListener("click", function(event){
+submitEl.addEventListener("click", async function(event){
     event.preventDefault()
     var Name = document.querySelector('#searchValue').value
     var locName={}
     locName.City = document.querySelector('#searchValue').value.split(',')[0].trim()
     locName.State = document.querySelector('#searchValue').value.split(',')[1].trim()
-    //only add city if it doesn't exist
-    if (!savedCities.Name){savedCities[Name] = locName}
-    localStorage.setItem("cityCache", JSON.stringify(savedCities))
-    populateCities()
+    var results = await getCoord(locName.City, locName.State)
+    console.log(results[0])
+    //if results returned continue, if not prompt user to try again
+    if (results.length > 0){
+        var weatherData = await getWeather(results[0].lat, results[0].lon)
+        //only add city if it doesn't exist
+        if (!savedCities.Name){savedCities[Name] = locName}
+        localStorage.setItem("cityCache", JSON.stringify(savedCities))
+        populateCities()
+    }else{
+        window.alert("That location could not be found. Please try again.")
+    }    
 })
 
 // function for populating saved cities
 function populateCities(){
     cityListEl.replaceChildren()
     Object.keys(savedCities).forEach((element)=>{
-        console.log(savedCities[element])
         var cityListItem = document.createElement("p")
         cityListItem.textContent = savedCities[element].City + ", " + savedCities[element].State
         cityListEl.appendChild(cityListItem)
@@ -46,8 +53,12 @@ function populateCities(){
 }
 
 //function for getting lat and long
-function getCoord(city, state){
-
+async function getCoord(city, state){
+    locURL = geoURL + `&q=${city},${state},1`
+    console.log(locURL)
+    let response = await fetch(locURL)
+    let data = await response.json()
+    return await data
 }
 
 populateCities()
